31156de51b5e49a4a6644f9a24f763e440852b6f
ludovic <ludovic@scilink.net>
2025-04-14 14:30:06 +0200

    WIP audio

--- src/audio.py
+++ src/audio.py
@@ -2,4 +2,5 @@ import pyaudio
 
 p = pyaudio.PyAudio()
 for ii in range(p.get_device_count()):
-    print(p.get_device_info_by_index(ii).get('name'))
+    name =p.get_device_info_by_index(ii).get('name')
+    print(f"index {ii} {name}")
