2f955208d7a999a26d7f322dc99b9ee3632ca995
ludovic <ludovic@scilink.net>
2025-06-03 13:14:09 +0100

    add detector stereo

--- src/beat_detector_stereo.py
+++ src/beat_detector_stereo.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(description="Détection de beats audio en conso
 parser.add_argument('--min_freq', type=float, default=0.0, help="Fréquence minimale à analyser (Hz)")
 parser.add_argument('--max_freq', type=float, default=6000.0, help="Fréquence maximale à analyser (Hz)")
 parser.add_argument('--min_energy', type=float, default=1e5, help="Seuil minimal absolu d'énergie pour ignorer les bandes faibles")
+parser.add_argument('--c_factor', type=float, default=2, help="Seuil du ratio E/<E> de détection")
 parser.add_argument('--debug', action='store_true')
 parser.set_defaults(debug=False)
 args = parser.parse_args()
@@ -19,17 +20,20 @@ args = parser.parse_args()
 CHUNK = 1024
 RATE = 96000
 SAMPLE_FORMAT = pyaudio.paInt32
-DEVICE_NAME = "ADCWM8782"
+#DEVICE_NAME = "ADCWM8782"
+DEVICE_NAME = "adc_sv"
 CHANNELS = 2
-N_BANDS = 64
+N_BANDS = 32
 ENERGY_HISTORY = 42
 
 # === Initialisation PyAudio ===
 p = pyaudio.PyAudio()
 # Sélection automatique d'un périphérique contenant "Loopback" ou autre
 def find_input_device(name_part="Loopback"):
+    print(f"search for device {name_part}")
     for i in range(p.get_device_count()):
         info = p.get_device_info_by_index(i)
+        print(f"device {i} info {info['name']}")
         if name_part.lower() in info['name'].lower() and info['maxInputChannels'] > 0:
             return i
     return None
@@ -69,6 +73,7 @@ def log_frequency_bands(n_freqs=CHUNK, n_bands=N_BANDS):
     return bands
 
 bands = log_frequency_bands()
+print(f"bands = {bands}")
 
 # Band filtering according to frequency
 def band_to_freq(index):
@@ -76,13 +81,22 @@ def band_to_freq(index):
 
 filtered_bands = []
 filtered_indices = []
+filtered_frequency = []
 for i, (start, end) in enumerate(bands):
     start_freq = band_to_freq(start)
     end_freq = band_to_freq(end)
-    if start_freq >= args.min_freq and end_freq <= args.max_freq:
+    if (start_freq <= args.min_freq and args.min_freq <= end_freq) or \
+            (args.min_freq <= start_freq and end_freq <= args.max_freq) or \
+            (start_freq <= args.max_freq and args.max_freq <= end_freq):
         filtered_bands.append((start, end))
         filtered_indices.append(i)
+        filtered_frequency.append((start_freq,end_freq))
 
+print("### filtered bands")
+print(f"bands = {filtered_bands}")
+print(f"frequency = {filtered_frequency}")
+print(f"indices (band number) = {filtered_indices}")
+      
 
 prev_beat = perf_counter()
 beats_empty = [0]*N_BANDS 
@@ -100,44 +114,36 @@ try:
 
         beats_detected = beats_empty
         #cfactor = []
-        for idx, iband in enumerate(filtered_indices):
-            start, end = filtered_bands[idx]
-            band_weight = (end - start) / CHUNK
+        for iband in filtered_indices:
+            start, end = bands[iband]
+            band_weight = ((end - start) + 1) / CHUNK
             energy = band_weight * np.sum(spectrum[start:end] ** 2)
             if energy < args.min_energy:
-                beat = False
                 energy_history[iband].append(energy)
-                #beat_detected[i] = 0
+                beats_detected[iband] = 0
                 continue
             energy_history[iband].append(energy)
 
             mean = np.mean(energy_history[iband])
             var = np.var(energy_history[iband])
-            current_cfactor = -15*var+1.55
-            beat = energy > current_cfactor * mean
-            beats_detected[iband] = (1 if beat else 0)
+            ratio = energy / mean
+            #current_cfactor = -15*var+1.55
+            beats_detected[iband] = (1 if ratio > args.c_factor  else 0)
             if args.debug:
-                print(f"Bande {iband:02d} | Energie: {energy_history[iband][-1]:.2e} | Moyenne: {mean:.2e} | Variance: {var:.2e} | Cfact: {current_cfactor:.2e} | Beat: {'OUI' if beats_detected[iband] else '-'}")
-               
+                print(f"Bande {iband:02d} | Energie: {energy_history[iband][-1]:.2e} | Moyenne: {mean:.2e} | Ratio {ratio:.2e} | Variance: {var:.2e} | Beat: {'OUI' if beats_detected[iband] else '-'}")
         if sum(beats_detected):
-            curr_time = perf_counter()
-            if curr_time - prev_beat > 60/180: # 180 BPM max
-                # reset the timer
-                prev_beat = curr_time
-            else:
-                beats_detected = beats_empty
-        
+           curr_time = perf_counter()
+           if curr_time - prev_beat < 60/180: # 180 BPM max
+               beats_detected = beats_empty
+           else:
+               prev_beat = curr_time
 
         
         # Affichage console : une ligne de 32 caractères représentant les beats
-        visual = ''.join(['#' if i in filtered_indices and beats_detected[i] else '-' for i in range(N_BANDS)])
+        visual = ''.join(['#' if beats_detected[i] else '-' for i in range(N_BANDS)])
         print(visual)
-        if args.debug:
-            print('-' * 80)
-            
-        
 
-        #time.sleep(0.03)  # ~30 FPS
+        time.sleep(0.03)  # ~30 FPS
 
 except KeyboardInterrupt:
     print("\n[INFO] Arrêt de l'analyse.")
