96d31db90ad534c4746c2754c89def1e773decac
ludovic <ludovic@scilink.net>
2025-06-04 16:11:20 +0200

    update gpio

--- src/Pins.py
+++ src/Pins.py
@@ -1,6 +1,15 @@
 import wiringpi
 
-""" GPIO 4 reserved for shutdown
+""" 
+    Reserved GPIO
+    GPIO 2    I2C SDA
+    GPIO 3    I2C SCL
+    GPIO 4     for shutdown
+    GPIO 18    I2S Bit clock
+    GPIO 19    I2S LR clock
+    GPIO 20    I2S data in
+    GPIO 21    I2S data out - not used -
+    
 """
 PINS = {
     'RS485_DE': 17,
