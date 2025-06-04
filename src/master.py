3507519674fe8e07aa81601a289c001b4852cb49
ludovic <ludovic@scilink.net>
2025-06-04 14:37:29 +0200

    correct log

--- src/master.py
+++ src/master.py
@@ -1,21 +1,23 @@
 import logging
 import queue
 import datetime
-
 import Config
 import CommunicationProcessor
 import BeatDetector
-
-
 import Pins
+from logging.handlers import RotatingFileHandler
 
+
+logger = logging.getLogger('my_logger')
+handler = RotatingFileHandler('my_log.log', maxBytes=1024, backupCount=5)
+logger.addHandler(handler)
 
 if __name__ == "__main__":
     config = Config.Config()
 
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    fh = logging.FileHandler(config.logFile)
+    fh = logging.RotatingFileHandler(config.logFile, maxBytes=102400, backupCount=2)
     fh.setLevel(logging.INFO)
     ch = logging.StreamHandler()
     ch.setLevel(logging.ERROR)
